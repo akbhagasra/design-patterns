99414ec3338259a87e94952bc0e047fe45154552
akbhagasra <abhagasra225@gmail.com>
2023-12-30 17:12:53 +0530

    added singleton design pattern

---
+++ CreationalDesignPatterns/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationalDesignPatterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Design Patterns project!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CreationalDesignPatterns/Singleton/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationalDesignPatterns.SingletonSpace
+{
+    public class Singleton
+    {
+        private static Singleton _instance;
+        private Singleton() 
+        {
+            Console.WriteLine("Singleton object initialized");
+        }
+
+        public static Singleton getInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+}
